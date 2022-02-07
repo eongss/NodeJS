190948279916e9409806a2f6059374d9969547ba
moon <xaria21@naver.com>
2022-02-07 23:24:02 +0900

    20220207

--- ex/createWriteStream.js
+++ ex/createWriteStream.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 
-const writeStream = fs.createWriteStream('./writeme2.txt');
+const writeStream = fs.createWriteStream('./ex/writeme2.txt');
 writeStream.on('finish', () => {
     console.log('파일 쓰기 완료');
 });
